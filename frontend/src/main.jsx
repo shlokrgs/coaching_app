0466b2a012774e6b5ed2c61fcf2c21484af967c1
Vikash Agarwal <vikashagarwal@Vikashs-MacBook-Pro.local>
2025-05-08 22:33:36 +0530

    Wrap App in BrowserRouter to fix useNavigate error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,19 @@
-// -- main.jsx --
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
-import { AuthProvider } from './Context/AuthContext';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
